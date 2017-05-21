92236a6cb11bd4f6247851866d35daaf156397ca
Simon Geilfus <simongeilfus@users.noreply.github.com>
2017-05-21 11:50:14 -0400

    Simplified Test.h/Test.cpp

--- tests/CompilerRewrite/src/Test.cpp
+++ tests/CompilerRewrite/src/Test.cpp
@@ -1,36 +1,12 @@
 #include "Test.h"
-#include "cinder/app/App.h"
-#include "cinder/gl/Batch.h"
-#include "cinder/gl/Shader.h"
-#include "cinder/gl/TextureFont.h"
-#include "cinder/gl/wrapper.h"
-#include "cinder/gl/scoped.h"
 
 using namespace std;
-using namespace ci;
-using namespace ci::app;
 
 Test::Test()
 {
-	mFont = gl::TextureFont::create( Font( "Arial", 35 ) );
-	mBatch = gl::Batch::create( geom::Circle().radius( 8.5f ).subdivisions( 32 ), gl::getStockShader( gl::ShaderDef().color() ) );
 }
 
-void Test::update()
+std::string Test::getString()
 {
-}
-
-void Test::draw()
-{
-	gl::clear( ColorA::gray( 0.85f ) );
-	
-	gl::ScopedColor textColor( ColorA::gray( 0.25f ) );
-	auto text = "Runtime Compiled c++";
-	auto textSize = mFont->measureString( text );
-	mFont->drawString( text, getWindowCenter() - vec2( textSize.x * 0.5f, 0.0f ) );
-	
-	gl::ScopedColor batchColor( ColorA::gray( 0.95f ) );
-	gl::ScopedModelMatrix scopedModel;
-	gl::translate( getWindowCenter() - vec2( textSize.x * 0.5f + 25.0f, 9.0f ) );
-	mBatch->draw();
+	return "Hello!";
 }
--- tests/CompilerRewrite/src/Test.h
+++ tests/CompilerRewrite/src/Test.h
@@ -1,20 +1,9 @@
 #pragma once
 
-#include <memory>
-
-// forward declare classes whenever it is possible
-// as it will make the class reloading faster
-namespace cinder { namespace gl {
-	typedef std::shared_ptr<class Batch> BatchRef;
-	typedef std::shared_ptr<class TextureFont> TextureFontRef;
-} } namespace ci = cinder;
+#include <string>
 
 class Test {
 public:
 	Test();
-	virtual void update();
-	virtual void draw();
-protected:
-	ci::gl::TextureFontRef	mFont;
-	ci::gl::BatchRef		mBatch;
+	virtual std::string getString();
 };
