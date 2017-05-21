19844665d299e0d259cf2f59bc2cff2ae323d492
Simon Geilfus <simongeilfus@users.noreply.github.com>
2017-05-21 11:32:45 -0400

    Updated CompilerRewriteApp with command example

--- tests/CompilerRewrite/src/CompilerRewriteApp.cpp
+++ tests/CompilerRewrite/src/CompilerRewriteApp.cpp
@@ -20,6 +20,35 @@ public:
 void CompilerRewriteApp::setup()
 {
 	mCompiler = rt::Compiler::create();
+	
+	// compiler args
+	std::string command = "cl ";
+	command += " /Fd\"C:\\CODE\\CINDER\\CINDER_FORK\\BLOCKS\\CINDER-RUNTIME\\TESTS\\COMPILERREWRITE\\VC2015\\BUILD\\X64\\DEBUG_SHARED\\INTERMEDIATE\\VC140_RT.PDB\" ";
+	command += " /Fo\"C:\\CODE\\CINDER\\CINDER_FORK\\BLOCKS\\CINDER-RUNTIME\\TESTS\\COMPILERREWRITE\\VC2015\\BUILD\\X64\\DEBUG_SHARED\\INTERMEDIATE\\\\\" "; 
+	command += "/Zi /nologo /W3 /WX- /Od /D CINDER_SHARED /D WIN32 /D _WIN32_WINNT=0x0601 /D _WINDOWS /D NOMINMAX /D _DEBUG /D _UNICODE /D UNICODE /Gm /EHsc /RTC1 /MDd /GS /fp:precise /Zc:wchar_t /Zc:forScope /Zc:inline /Gd /TP ";
+	command += "/I..\\INCLUDE /I..\\..\\..\\..\\..\\INCLUDE /I..\\..\\..\\INCLUDE /I..\\BLOCKS\\WATCHDOG\\INCLUDE ";
+	command += " C:\\CODE\\CINDER\\CINDER_FORK\\BLOCKS\\CINDER-RUNTIME\\TESTS\\COMPILERREWRITE\\SRC\\TEST.CPP";
+
+	// linker args
+	command += " /link ";
+	command += " /OUT:test.dll";
+	command += " /IMPLIB:test.lib";
+	command += " /INCREMENTAL:NO /NOLOGO";
+	command += " /LIBPATH:..\\..\\..\\..\\..\\LIB\\MSW\\X64";
+	command += " /LIBPATH:..\\..\\..\\..\\..\\LIB\\MSW\\X64\\DEBUG_SHARED\\V140\\";
+	command += " CINDER.LIB OPENGL32.LIB KERNEL32.LIB USER32.LIB GDI32.LIB WINSPOOL.LIB COMDLG32.LIB ADVAPI32.LIB SHELL32.LIB OLE32.LIB OLEAUT32.LIB UUID.LIB ODBC32.LIB ODBCCP32.LIB /NODEFAULTLIB:LIBCMT /NODEFAULTLIB:LIBCPMT ";
+	command += " /MACHINE:X64";
+	//command += " /CGTHREADS:8";
+#if defined( _DEBUG )
+	command += " /PDB:\"C:\\CODE\\CINDER\\CINDER_FORK\\BLOCKS\\CINDER-RUNTIME\\TESTS\\COMPILERREWRITE\\VC2015\\BUILD\\X64\\DEBUG_SHARED\\INTERMEDIATE\\VC140_RT.PDB\"";
+	command += " /DEBUG:FASTLINK";
+#endif
+	command += " /DLL ";
+	//command += " C:\\CODE\\CINDER\\CINDER_FORK\\BLOCKS\\CINDER-RUNTIME\\TESTS\\COMPILERREWRITE\\VC2015\\BUILD\\X64\\DEBUG_SHARED\\INTERMEDIATE\\TEST.OBJ";
+
+	mCompiler->build( command, []( const rt::CompilationResult &result ) {
+		app::console() << "Done!" << endl;
+	} );
 }
 
 void CompilerRewriteApp::update()
