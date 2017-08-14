8fdd2e508c2361bba55a4581cbf118cd85818864
Simon Geilfus <simongeilfus@users.noreply.github.com>
2017-08-14 09:04:10 -0400

    Added ClassFactory helper function

---
+++ src/runtime/ClassFactory.cpp
@@ -0,0 +1,26 @@
+#include "runtime/ClassFactory.h"
+#include <fstream>
+
+using namespace std;
+using namespace ci;
+
+namespace runtime {
+
+void generateClassFactory( const ci::fs::path &outputPath, const std::string &className, const std::string &headerName = "" )
+{
+	std::ofstream outputFile( outputPath );		
+	outputFile << "#include <memory>" << endl << endl;
+	outputFile << "#include \"" << ( headerName.empty() ? className + ".h" : headerName ) << "\"" << endl << endl;
+	outputFile << "extern \"C\" __declspec(dllexport) void __cdecl rt_make_shared( std::shared_ptr<" << className << ">* ptr )" << endl;
+	outputFile << "{" << endl;
+	outputFile << "\t*ptr = std::make_shared<" << className << ">();" << endl;
+	outputFile << "}" << endl;
+	outputFile << endl;
+	outputFile << "extern \"C\" __declspec(dllexport) void __cdecl rt_make_unique( std::unique_ptr<" << className << ">* ptr )" << endl;
+	outputFile << "{" << endl;
+	outputFile << "\t*ptr = std::make_unique<" << className << ">();" << endl;
+	outputFile << "}" << endl;
+	outputFile << endl;
+}
+
+}
