aa5690e19432495f7a673e114dd40d0a1f7591fc
Simon Geilfus <simongeilfus@users.noreply.github.com>
2017-08-14 23:58:17 -0400

    Added watchClassInstance template + cleaned up new prototype

--- tests/CompilerRewrite/src/Test2.cpp
+++ tests/CompilerRewrite/src/Test2.cpp
@@ -1,5 +1,6 @@
 #include "Test2.h"
 #include "cinder/app/App.h"
+#include "runtime/ClassInstanceWatcher.h"
 
 using namespace std;
 using namespace ci;
@@ -12,3 +13,17 @@ std::string Test2::getString()
 {
 	return "Hello!";
 }
+
+#if ! defined( RT_COMPILED ) && defined( CINDER_SHARED )
+void* Test2::operator new( size_t size )
+{
+	void * ptr = ::operator new( size );
+	rt::watchClassInstance( static_cast<Test2*>( ptr ), { CI_RT_PROJECT_ROOT / "src/Test2.cpp", CI_RT_PROJECT_ROOT / "src/Test2.h" }, CI_RT_INTERMEDIATE_DIR / "runtime/Test2/Test2.dll" );
+	return ptr;
+}
+	
+void Test2::operator delete( void* ptr )
+{
+	::operator delete( ptr );
+}
+#endif
