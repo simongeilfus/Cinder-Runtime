ada2bc345d26bd68425d7eeb82613c6e8b5a590e
Simon Geilfus <simongeilfus@users.noreply.github.com>
2017-08-15 23:00:25 -0400

    Renamed ClassInstanceWatcher to ClassWatcher + Added new/delete Preprocessor

---
+++ include/runtime/ClassWatcher.h
@@ -0,0 +1,161 @@
+#pragma once
+
+#if ! defined( RT_COMPILED ) && defined( CINDER_SHARED )
+
+#include "cinder/Filesystem.h"
+#include "cinder/FileWatcher.h"
+#include "cinder/Timer.h"
+
+#include "runtime/Module.h"
+#include "runtime/CompilerMsvc.h"
+
+namespace runtime {
+
+template<class T>
+void watchClassInstance( T* ptr, const std::vector<ci::fs::path> &filePaths, const ci::fs::path &dllPath, const rt::Compiler::BuildSettings &settings = rt::Compiler::BuildSettings(), bool unwatch = false )
+{
+	static rt::ModulePtr sModule;
+	static std::vector<T*> sInstances;
+
+	if( unwatch ) {
+		sInstances.erase( std::remove( sInstances.begin(), sInstances.end(), ptr ), sInstances.end() );
+		return;
+	}
+	else {
+		sInstances.push_back( static_cast<T*>( ptr ) );
+	}
+
+	if( ! sModule ) {
+		sModule = std::make_unique<rt::Module>( dllPath );
+		ci::fs::path source = filePaths.front();
+		ci::FileWatcher::instance().watch( filePaths, 
+			ci::FileWatcher::Options().callOnWatch( false ),
+			[&,source,settings]( const ci::WatchEvent &event ) {  
+				ci::Timer timer( true );
+				// unlock the dll-handle before building
+				sModule->unlockHandle();
+		
+				// initiate the build
+				rt::CompilerMsvc::instance().build( source, settings, [&,event,timer]( const rt::CompilationResult &result ) {
+					// if a new dll exists update the handle
+					if( ci::fs::exists( sModule->getPath() ) ) {
+						sModule->updateHandle();
+						// and try to get a ptr to its make_unique factory function
+						if( auto makeRaw = sModule->getMakeRawFactory<T>() ) {
+#if 1
+							if( event.getFile().extension() == ".cpp" ) {
+								// create a single new instance an use its vtable to override the originals's vtables
+								T* newPtr = makeRaw();
+								for( size_t i = 0; i < sInstances.size(); ++i ) {
+									*(void **)sInstances[i] = *(void**) newPtr;
+								}
+								::operator delete( newPtr );
+							}
+							else if( event.getFile().extension() == ".h" ){
+								// create new instances and swap with the originals
+								for( size_t i = 0; i < sInstances.size(); ++i ) {
+									T* newPtr = makeRaw();
+									size_t size = sizeof T;
+									void* temp = malloc( size );
+									memcpy( temp, newPtr, size );
+									memcpy( newPtr, sInstances[i], size );
+									memcpy( sInstances[i], temp, size );
+								
+									::operator delete( newPtr );
+								}
+							}
+
+							ci::app::console() << timer.getSeconds() * 1000.0 << "ms" << std::endl;
+#elif 0
+							// create a single new instance an use its vtable to override the originals's vtables
+							T* newPtr = makeRaw();
+							ci::app::console() << "vtable size " << sizeof( *(void **) newPtr ) << std::endl;
+							ci::app::console() << "object size " << sizeof( * newPtr ) << std::endl;
+							for( size_t i = 0; i < sInstances.size(); ++i ) {
+								*(void **)sInstances[i] = *(void**) newPtr;
+							}
+							::operator delete( newPtr );
+#else
+							// create new instances and swap with the originals
+							for( size_t i = 0; i < sInstances.size(); ++i ) {
+								T* newPtr = makeRaw();
+								size_t size = sizeof T;
+								void* temp = malloc( size );
+								memcpy( temp, newPtr, size );
+								memcpy( newPtr, sInstances[i], size );
+								memcpy( sInstances[i], temp, size );
+								
+								::operator delete( newPtr );
+							}
+#endif
+						}
+			
+					}
+				} );
+			} 
+		);
+	}
+}
+
+} // namespace runtime
+
+namespace rt = runtime;
+
+#include "cinder/app/App.h"
+
+#define RT_WATCH_CLASS_HEADER \
+public: \
+	void* operator new( size_t size ); \
+	void operator delete( void* ptr ); \
+	static const ci::fs::path getHeaderPath() { return ci::fs::absolute( ci::fs::path( __FILE__ ) ); } \
+
+#define __RT_WATCH_CLASS_IMPL0( Class ) \
+void* Class::operator new( size_t size ) \
+{ \
+	void * ptr = ::operator new( size ); \
+	auto cppPath = ci::fs::absolute( ci::fs::path( __FILE__ ) ); \
+	rt::watchClassInstance( static_cast<Class*>( ptr ), { cppPath, getHeaderPath() }, CI_RT_INTERMEDIATE_DIR / "runtime" / std::string( #Class ) / ( std::string( #Class ) + ".dll" ), rt::Compiler::BuildSettings().default().define( "RT_COMPILED" ).include( "../../../include" ) );\
+	return ptr; \
+} \
+void Class::operator delete( void* ptr ) \
+{ \
+	rt::watchClassInstance( static_cast<Class*>( ptr ), {}, "", rt::Compiler::BuildSettings(), true ); \
+	::operator delete( ptr ); \
+} \
+
+#define __RT_WATCH_CLASS_IMPL1( Class, Settings ) \
+void* Class::operator new( size_t size ) \
+{ \
+	void * ptr = ::operator new( size ); \
+	auto cppPath = ci::fs::absolute( ci::fs::path( __FILE__ ) ); \
+	rt::watchClassInstance( static_cast<Class*>( ptr ), { cppPath, getHeaderPath() }, CI_RT_INTERMEDIATE_DIR / "runtime" / std::string( #Class ) / ( std::string( #Class ) + ".dll" ), Settings );\
+	return ptr; \
+} \
+void Class::operator delete( void* ptr ) \
+{ \
+	rt::watchClassInstance( static_cast<Class*>( ptr ), {}, "", rt::Compiler::BuildSettings(), true ); \
+	::operator delete( ptr ); \
+} \
+
+#define __RT_WATCH_CLASS_IMPL2( Class, Header, Source, Dll, Settings ) \
+void* Class::operator new( size_t size ) \
+{ \
+	void * ptr = ::operator new( size ); \
+	rt::watchClassInstance( static_cast<Class*>( ptr ), { Source, Header }, Dll, Settings );\
+	return ptr; \
+} \
+void Class::operator delete( void* ptr ) \
+{ \
+	rt::watchClassInstance( static_cast<Class*>( ptr ), {}, "", rt::Compiler::BuildSettings(), true ); \
+	::operator delete( ptr ); \
+} \
+
+#define __RT_WATCH_CLASS_IMPL_SWITCH(_1,_2,_3,NAME,...) NAME
+#define RT_WATCH_CLASS_IMPL( ... ) __RT_WATCH_CLASS_IMPL_SWITCH(__VA_ARGS__,__RT_WATCH_CLASS_IMPL2,__RT_WATCH_CLASS_IMPL1,__RT_WATCH_CLASS_IMPL0)(__VA_ARGS__)
+
+#else
+
+#define RT_WATCH_CLASS_HEADER
+#define RT_WATCH_CLASS_IMPL( ... )
+
+#endif
