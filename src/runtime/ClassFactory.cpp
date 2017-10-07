0e02ecac5f064ef17a5183417880b576ba9e3d94
Simon Geilfus <simongeilfus@users.noreply.github.com>
2017-10-07 00:21:24 -0400

    Cleaned up ClassFactory code generation

--- src/runtime/ClassFactory.cpp
+++ src/runtime/ClassFactory.cpp
@@ -9,26 +9,15 @@ namespace runtime {
 void generateClassFactory( const ci::fs::path &outputPath, const std::string &className, const std::string &headerName )
 {
 	std::ofstream outputFile( outputPath );		
-	outputFile << "#include <memory>" << endl << endl;
 	outputFile << "#include \"" << ( headerName.empty() ? className + ".h" : headerName ) << "\"" << endl << endl;
-	outputFile << "extern \"C\" __declspec(dllexport) void __cdecl rt_make_shared( std::shared_ptr<" << className << ">* ptr )" << endl;
-	outputFile << "{" << endl;
-	outputFile << "\t*ptr = std::make_shared<" << className << ">();" << endl;
-	outputFile << "}" << endl;
-	outputFile << endl;
-	outputFile << "extern \"C\" __declspec(dllexport) void __cdecl rt_make_unique( std::unique_ptr<" << className << ">* ptr )" << endl;
-	outputFile << "{" << endl;
-	outputFile << "\t*ptr = std::make_unique<" << className << ">();" << endl;
-	outputFile << "}" << endl;
-	outputFile << endl;
-	outputFile << "extern \"C\" __declspec(dllexport) " << className << "* __cdecl rt_make_raw()" << endl;
+	outputFile << "extern \"C\" __declspec(dllexport) " << className << "* __cdecl rt_new_operator()" << endl;
 	outputFile << "{" << endl;
 	outputFile << "\treturn new " << className << "();" << endl;
 	outputFile << "}" << endl;
 	outputFile << endl;
-	outputFile << "extern \"C\" __declspec(dllexport) size_t __cdecl rt_sizeof()" << endl;
+	outputFile << "extern \"C\" __declspec(dllexport) " << className << "* __cdecl rt_placement_new_operator(  " << className << "* address )" << endl;
 	outputFile << "{" << endl;
-	outputFile << "\treturn sizeof( " << className << ");" << endl;
+	outputFile << "\treturn new (address)" << className << "();" << endl;
 	outputFile << "}" << endl;
 	outputFile << endl;
 }
