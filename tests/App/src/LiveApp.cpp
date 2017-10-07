8f66818ffc4a0d85085162e8ef420b9bd194fc13
Simon Geilfus <simongeilfus@users.noreply.github.com>
2017-10-07 19:57:39 -0400

    tests/App: Made LiveApp.cpp slightly more interesting

--- tests/App/src/LiveApp.cpp
+++ tests/App/src/LiveApp.cpp
@@ -1,6 +1,7 @@
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
+#include "cinder/CameraUi.h"
 
 #include "runtime/App.h"
 
@@ -10,12 +11,48 @@ using namespace std;
 
 class LiveApp : public App {
 public:
+	LiveApp();
+	void setup() override;
 	void draw() override;
+
+	CameraPersp mCamera;
+	CameraUi mCameraUi;
+	gl::BatchRef mBatch;
 };
+
+LiveApp::LiveApp()
+: mCamera( CameraPersp( getWindowWidth(), getWindowHeight(), 60.0f, 0.1f, 100.0f ).calcFraming( Sphere( vec3( 0.0f ), 10.0f ) ) ),
+mCameraUi( &mCamera, getWindow() )
+{
+}
+
+const string vert = R"(
+uniform mat4 ciModelViewProjection;
+in vec4 ciPosition;
+void main( void )
+{
+	gl_Position = ciModelViewProjection * ciPosition;
+}
+)";
+const string frag = R"(
+out vec4 oColor;
+void main( void )
+{
+	oColor = vec4( 1, 1, 1, 1 );
+}
+)";
+
+void LiveApp::setup()
+{
+	auto glslProg = gl::GlslProg::create( vert, frag );
+	mBatch = gl::Batch::create( geom::Cube(), glslProg );
+}
 
 void LiveApp::draw()
 {
-	gl::clear( Color::gray( 0.5f ) );
+	gl::clear( Color::gray( 0.125f ) );
+	gl::setMatrices( mCamera );
+	mBatch->draw();
 }
 
-CINDER_RUNTIME_APP( LiveApp, RendererGl )
+CINDER_RUNTIME_APP( LiveApp, RendererGl( RendererGl::Options().msaa( 8 ) ) )
