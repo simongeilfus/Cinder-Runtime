132e23a459b719351f212d9bb9e5e9fa9d69cf07
Simon Geilfus <simongeilfus@users.noreply.github.com>
2017-10-24 09:25:16 -0400

    Remove make_shared ambiguous pp

--- include/runtime/make_shared.h
+++ include/runtime/make_shared.h
@@ -23,7 +23,3 @@ inline std::shared_ptr<T> make_shared( Args&&... args )
 } // namespace runtime
 
 namespace rt = runtime;
-
-#if defined( CINDER_SHARED ) && ! defined( RT_DISABLE_MAKE_SHARED_PP )
-#define make_shared rt::make_shared
-#endif
