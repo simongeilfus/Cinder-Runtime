2e4367f729ffd77c5756104ccc64497d8e535d12
Simon Geilfus <simongeilfus@users.noreply.github.com>
2017-11-14 14:27:11 +0000

    Making ClassFactory type agnostic

--- src/runtime/ClassFactory.cpp
+++ src/runtime/ClassFactory.cpp
@@ -15,9 +15,9 @@ void generateClassFactory( const ci::fs::path &outputPath, const std::string &cl
 	outputFile << "\treturn new " << className << "();" << endl;
 	outputFile << "}" << endl;
 	outputFile << endl;
-	outputFile << "extern \"C\" __declspec(dllexport) " << className << "* __cdecl rt_placement_new_operator(  " << className << "* address )" << endl;
+	outputFile << "extern \"C\" __declspec(dllexport) void* __cdecl rt_placement_new_operator(  void* address )" << endl;
 	outputFile << "{" << endl;
-	outputFile << "\treturn new (address)" << className << "();" << endl;
+	outputFile << "\treturn static_cast<void*>( new (address) " << className << "() );" << endl;
 	outputFile << "}" << endl;
 	outputFile << endl;
 }
